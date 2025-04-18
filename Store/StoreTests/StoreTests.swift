e9edc575dbd4d0c7df9718bee37b147309934573
Jessica Wang <114797804+jessicawanggg@users.noreply.github.com>
2025-04-17 18:42:03 -0700

    Extra credit completed

--- Store/StoreTests/StoreTests.swift
+++ Store/StoreTests/StoreTests.swift
@@ -33,6 +33,7 @@ final class StoreTests: XCTestCase {
 Receipt:
 Beans (8oz Can): $1.99
 ------------------
+TAX: $0.00
 TOTAL: $1.99
 """
         XCTAssertEqual(expectedReceipt, receipt.output())
@@ -50,7 +51,7 @@ TOTAL: $1.99
         XCTAssertEqual(199, register.subtotal())
         register.scan(Item(name: "Pencil", priceEach: 99))
         XCTAssertEqual(298, register.subtotal())
-        register.scan(Item(name: "Granols Bars (Box, 8ct)", priceEach: 499))
+        register.scan(Item(name: "Granola Bars (Box, 8ct)", priceEach: 499))
         XCTAssertEqual(797, register.subtotal())
         
         let receipt = register.total()
@@ -60,10 +61,32 @@ TOTAL: $1.99
 Receipt:
 Beans (8oz Can): $1.99
 Pencil: $0.99
-Granols Bars (Box, 8ct): $4.99
+Granola Bars (Box, 8ct): $4.99
 ------------------
+TAX: $0.00
 TOTAL: $7.97
 """
+        XCTAssertEqual(expectedReceipt, receipt.output())
+    }
+    
+    //Extra credit
+    func testTwoForOnePricing() {
+        register.scan(Item(name: "Beans (8oz Can)", priceEach: 199, isTwoForOne: true))
+        register.scan(Item(name: "Beans (8oz Can)", priceEach: 199, isTwoForOne: true))
+        register.scan(Item(name: "Beans (8oz Can)", priceEach: 199, isTwoForOne: true))
+
+        XCTAssertEqual(199 * 2, register.subtotal())
+
+        let receipt = register.total()
+        let expectedReceipt = """
+        Receipt:
+        Beans (8oz Can): $1.99
+        Beans (8oz Can): $1.99
+        Beans (8oz Can): $1.99
+        ------------------
+        TAX: $0.00
+        TOTAL: $3.98
+        """
         XCTAssertEqual(expectedReceipt, receipt.output())
     }
 }
